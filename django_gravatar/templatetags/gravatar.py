f7d6bd1cbe0ff9f0a6fb97323e7c9adfbd58a3d6
Tristan Waddington <tristan.waddington@gmail.com>
2011-07-19 00:19:47 -0700

    Add docstrings to stub gravatar templatetags.

--- django_gravatar/templatetags/gravatar.py
+++ django_gravatar/templatetags/gravatar.py
@@ -3,21 +3,22 @@ from django import template
 # Get template.Library instance
 register = template.Library()
 
-def gravatar_url(email, size, default, rating, secure):
+def gravatar_url(email, size):
+    """ Builds a gravatar url from an email """
     # ...
     pass
 
 def gravatar_img(email, size, default, rating, secure):
+    """ Builds an gravatar <img> tag from an email """
     # ...
     pass
 
 def gravatar(*args, **kwargs):
+    """ Alias for gravatar_img """
     # ...
     return gravatar_img(*args, **kwargs)
     
 
 register.simple_tag(gravatar_url)
 register.simple_tag(gravatar_img)
-
-# Simple alias
 register.simple_tag(gravatar)
