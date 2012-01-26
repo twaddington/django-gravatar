f3bdc6c727ee3e1adfdf0c08d9754e6c72358840
Tristan Waddington <tristan.waddington@gmail.com>
2012-01-26 14:02:35 -0800

    Change setup.py name to allow for pypi inclusion. Bump version to 1.0.0.

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from distutils.core import setup
 
 setup(
-    name='django-gravatar',
-    version='0.1.0',
+    name='django-gravatar2',
+    version='1.0.0',
     description='Basic Gravatar support with helper methods and templatetags.',
+    long_description='',
     author='Tristan Waddington',
     author_email='tristan.waddington@gmail.com',
     url='https://github.com/twaddington/django-gravatar',
     packages=['django_gravatar', 'django_gravatar.templatetags'],
     classifiers=[
-        'Development Status :: 3 - Alpha', # 4 Beta, 5 Production/Stable
+        'Development Status :: 5 - Production/Stable', # 4 Beta, 5 Production/Stable
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
